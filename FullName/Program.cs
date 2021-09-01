ef112d612504d5cd5e3c41dd8511e7dcd3fcd2e3
Opilane TTHK <opilane@tthk.ee>
2021-09-01 12:14:28 +0300

    Read Inputs

---
+++ FullName/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FullName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Step 1. programm.küsib kasutaja käest eesnime
+            //Step 2. programm.küsib kasutaja käest perekonnanime
+            //Step 3. programm.tervitab kasutajat kasutades neid andmeid
+
+
+            Console.WriteLine("What is your first name?");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string LastName = Console.ReadLine();
+
+            Console.WriteLine($"Hello, {FirstName} {LastName} !");
+
+        }
+    }
+}
